75a6b96641a334fe971169612618eafed1925ea3
Tobias Zürcher <tobias.zuercher@gmail.com>
2013-08-20 10:19:16 +0200

    java dubletten

---
+++ DublettenCheck/2013-06-12_Kata_Dubletten_Java/Kata1b/test/KataTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class KataTest {
+
+	@Test
+	public void firstTest() {
+		assertEquals(2 + 1, 3);
+	}
+
+}
---
+++ DublettenCheck/2013-08-14_Kata_Dubletten_Java/Kata3b/test/kata/FirstTest.java
@@ -0,0 +1,14 @@
+package kata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
